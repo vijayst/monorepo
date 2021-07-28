2a422218bca0296b081136638c1ff271d69e38c9
Vijay T <vijayst@gmail.com>
2021-07-28 17:18:56 +0530

    namespace packages with a @vt

--- packages/project1/src/App.tsx
+++ packages/project1/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Greeting } from 'shared';
+import { Greeting } from '@vt/shared';
 
 function App() {
   return (
